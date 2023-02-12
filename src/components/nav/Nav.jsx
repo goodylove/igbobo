56f69dea280c413daa9ab29c03c9be023a1ef8ea
goodness <goodyc474@gmail.com>
2023-02-12 02:45:35 +0100

    completed monster

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -16,7 +16,20 @@ const Nav = () => {
       </div>
       <div className="menu-con">
         <ul className="list">
-          <li className="shop-shop">Shop</li>
+          <li className="shop-shop">
+            Shop
+            <hr
+              style={{
+                // marginTop: "1rem",
+                position: "absolute",
+                top: "42px",
+                width: "42px",
+                color: "green",
+                backgroundColor: "green",
+                height: "2px",
+              }}
+            />
+          </li>
           <li>Plant care</li>
           <li>Worh-shop</li>
           <li>Blog</li>
