927db38b14d05d2d1f0572ec80f953ce76aa03dc
goodness <goodyc474@gmail.com>
2023-02-28 12:19:24 +0100

    created mobile view item

---
+++ src/components/MobileViewItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCart from "./addToCart/AddToCart";
+
+const MobileViewItem = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  );
+};
+
+export default MobileViewItem;
